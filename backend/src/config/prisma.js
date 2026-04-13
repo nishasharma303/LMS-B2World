3038759741859c6224b3a6747731a36302c9de25
nishasharma303 <nishaanshu2005@gmail.com>
2026-04-14 00:41:13 +0530

    fix: prisma esm import issue

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,6 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import pkg from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { env } from "./env.js";
+
+const { PrismaClient } = pkg;
 
 const adapter = new PrismaPg({
   connectionString: env.databaseUrl,
